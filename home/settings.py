3156603540adfcab9a282845074f99f7991a7616
Diego Gallo Valenzuela <diegogallovalenzuela@MacBook-Pro-de-Diego-2.local>
2022-08-25 12:00:44 -0500

    first commit

--- home/settings.py
+++ home/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'home.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, '.next/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
