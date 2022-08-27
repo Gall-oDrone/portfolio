3163dbc015f5c2f207d6f9b488e4d2cb288fd7be
Diego Gallo Valenzuela <diegogallovalenzuela@MacBook-Pro-de-Diego-2.local>
2022-08-27 18:48:23 -0500

    minor changes

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,11 @@
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost','127.0.0.1'],
+    domains: ['localhost','127.0.0.1', 'dgallo-portfolio.netlify.app/'],
   },
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([optimizedImages,nextConfig]);
