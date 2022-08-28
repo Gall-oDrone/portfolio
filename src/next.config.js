4ae73df7d51214d754cbae395145df162e9216ba
Diego Gallo Valenzuela <diegogallovalenzuela@MacBook-Pro-de-Diego-2.local>
2022-08-27 19:34:39 -0500

    minor changes

--- src/next.config.js
+++ src/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost','127.0.0.1', 'dgallo-portfolio.netlify.app', 'https://dgallo-portfolio.netlify.app/'],
+    domains: ['localhost','127.0.0.1', 'dgallo-portfolio.netlify.app', 'https://dgallo-portfolio.netlify.app/', 'dgallov-portfolio.vercel.app','https://dgallov-portfolio.vercel.app/'],
   },
 }
 
