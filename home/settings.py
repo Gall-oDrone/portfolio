670fcf1f22f6c894e0ff2c101f055b460b9e0124
Diego Gallo Valenzuela <diegogallovalenzuela@MacBook-Pro-de-Diego-2.local>
2022-08-29 09:44:34 -0500

    minor changes

--- home/settings.py
+++ home/settings.py
@@ -146,14 +146,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-if USE_S3:
+if not USE_S3:
     # AWS
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
     AWS_DEFAULT_ACL = None
-    AWS_S3_CUSTOM_DOMAIN = f'portfolio-dgallov-bucket.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
     PUBLIC_MEDIA_LOCATION = 'media'
